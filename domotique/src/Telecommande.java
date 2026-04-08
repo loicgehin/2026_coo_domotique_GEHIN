5792443840439248bbe09d30437b7e2fccf591c6
Loïc Géhin <loicxgehin@gmail.com>
2026-04-08 09:34:15 +0200

    création et completion télecommande et appareil étant une interface implémenté par Lampe

--- domotique/src/Telecommande.java
+++ domotique/src/Telecommande.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Telecommande {
+    public Telecommande() {
+        throw new Error ( " code non ecrit " );
+    }
+
+    public void ajouterAppareil(Appareil ap) {
+        throw new Error ( " code non ecrit " );
+    }
+
+    public void activerAppareil(int i) {
+        throw new Error ( " code non ecrit " );
+    }
+
+    public void desactiverAppareil(int i) {
+        throw new Error ( " code non ecrit " );
+    }
+
+    public void activerTout(){
+        throw new Error ( " code non ecrit " );
+    }
+
 }
